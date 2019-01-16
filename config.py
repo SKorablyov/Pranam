2cfc997e3475a11502b22798cb57047935abe507
mkorablyov <mkorablyov@gmail.com>
2019-01-16 17:54:10 -0500

    pranam seems to work

--- config.py
+++ config.py
@@ -42,17 +42,18 @@ class cfg3:
         # network and opt pars
         self.func = networks.mnist_fcnet
         self.func_pars = [1]
-        self.num_clones = 2
+        self.num_clones = 2#5 # fixme 256
         self.optimizer = tf.train.GradientDescentOptimizer
         self.learning_rate = 1e-5
-        self.batch_size = 2
+        self.batch_size = 100
         self.num_epochs = 50
         # embedding pars
         self.embed_vars = ["mnist_fcnet/w2:0"]
         self.embedder = embedders.FCEmbedder
-        embedding_shape = [None, 50, None]
-        embedding_inits = [tf.random_normal_initializer,tf.random_uniform_initializer,tf.random_uniform_initializer]
-        scaled_tanh = embedders.ScaledTanh("sctanh1", self.sess, init_a=1.0, train_a=False, init_b=0.01, train_b=False).doit
+        embedding_shape = [None, 256, None]
+        embedding_inits = [tf.contrib.layers.xavier_initializer(),tf.contrib.layers.xavier_initializer(),
+                           tf.contrib.layers.xavier_initializer()]
+        scaled_tanh = embedders.ScaledTanh("sctanh1", self.sess, init_a=1.0, train_a=False, init_b=0.01, train_b=False).doit # fixme trainabe
         embedding_acts = [tf.nn.relu, scaled_tanh]
         self.embedder_pars = [embedding_shape, embedding_inits, embedding_acts]
 
